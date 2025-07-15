302355baee747fcdd24aa504f39bbd31a9154122
shashikant kumar <shashi94goswami@gmail.com>
2025-07-15 20:44:26 +0530

    working with variable

---
+++ working-with-variable/brandname.py
@@ -0,0 +1,5 @@
+pet = input("what is your pet Name ? \n")
+city = input("What is your city  Name ? \n")
+
+
+print("your city name is " + city + " and pet name is " + pet)
---
+++ working-with-variable/breakline.py
@@ -0,0 +1,12 @@
+print("Hello Word, Hi how are you")
+print("Hello Word, \nHi how are you")
+
+# string concatenation
+
+print("Hello" + " " + "World")
+
+# input in python
+
+name = input("What is your name ?")
+print(name +" !")
+print(len(name))
